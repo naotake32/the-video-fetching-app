fa8927dbfd3b5c976dd6c615e455865be6b51307
github naotake32 <take32.nao@gmail.com>
2022-08-07 13:32:57 -0700

    First commit for the video-fetching-App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Api from './components/Api';
 
-function App() {
+
+
+
+export const App = () => {
+
+ const [inputKeyword, setInputKeyword] = useState("");
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Video-Fetching</h1>
+        <div className='input-wrapper'>
+          <p>enter the keyword</p>
+          <input onChange={(e) => {
+            console.log(e.target.value);
+            return setInputKeyword(e.target.value);
+          }} type="text" />
+
+        </div>
+        <Api
+        inputKeyword={inputKeyword}
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Api.tsx
@@ -0,0 +1,64 @@
+import React, {useState, useEffect } from "react";
+
+type ParamType = {
+    inputKeyword:string;
+  } 
+
+const YOUTUBE_SEARCH_API_URI = "https://www.googleapis.com/youtube/v3/search?";
+const API_KEY = "AIzaSyAGcq6cYFwovcxEQhvZrFZOgiXUlMF0Ukg";
+
+const Api = (props:ParamType) => {
+    
+    const {inputKeyword} = props;
+    const [videoId, setVideoId] = useState("");
+
+    const onClickSearch =() => {
+        console.log("sent to the param:", inputKeyword);
+      }
+
+    useEffect(() =>{
+
+    //define the query string
+    const params = {
+        key:API_KEY,
+        q:inputKeyword,
+        type:"video",
+        maxResults:"1",
+        order: "viewCount",
+    }
+    const queryParams = new URLSearchParams(params);
+
+   
+        fetch(YOUTUBE_SEARCH_API_URI + queryParams)
+        .then((res) => res.json())
+        .then(
+            (result) => {
+                console.log("API success:" , result);
+
+                if(result.items && result.items.length !== 0){
+                    const firstItem = result.items[0];
+                    setVideoId(firstItem.id.videoId);
+                }
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    },[])
+
+
+    return (
+        <>
+            <button onClick={onClickSearch}>search</button>
+            <iframe
+                id="player"
+                width="640"
+                height="360"
+                src={"https://www.youtube.com/embed/" + videoId}
+                frameBorder = "0"
+                allowFullScreen/>
+        </>
+    );
+};
+
+export default Api;
